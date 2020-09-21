8b38c03f66cec7146bf4905fd96a5da8bbc47c52
giantnuzul <giantnuzul@student.telkomuniversity.ac.id>
2020-09-21 14:58:13 +0700

    tes2.java

---
+++ praktiku3/tes2.java
@@ -0,0 +1,8 @@
+public class tes2{
+   public static void main(String[]args){ 
+      int umur = 18;
+      String name = "Giant";
+      System.out.println("Halo," + name);
+      System.out.println("Ternyata usiamu baru " + umur + " tahun ya");
+   }
+}
